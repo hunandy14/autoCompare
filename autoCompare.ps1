1133be3f88e5bd6514168251261f2f8b723599bb
Charlotte.Hong <hunandy14@gmail.com>
2021-12-03 01:05:15 +0900

    完成自動輸出比對html報告

--- autoCompare.ps1
+++ autoCompare.ps1
@@ -1,16 +1,24 @@
 function getCommitFile {
     param (
+        [Parameter(Position = 0)]
         [string] $CM1,
+        [Parameter(Position = 1)]
         [string] $CM2,
+        [Parameter(Position = 2)]
         [string] $gitDir,
+        [Parameter(ParameterSetName = "")]
+        [string] $dstDir,
+        [Parameter(ParameterSetName = "")]
+        [string] $ListFileName,
         [switch] $Expand
     )
-    $CmDir = "CommitFile"
+    if ($dstDir -eq "") { $dstDir = "$PSScriptRoot\CommitFiles" }
+    if ($ListFileName -eq "") { $ListFileName = "diff-list.txt" }
     # 獲取差異清單
     Set-location $gitDir
-    $F1 = "$PSScriptRoot\$CmDir\$CM1.zip".Replace("\", "/")
+    $F1 = "$dstDir\$CM1.zip".Replace("\", "/")
     New-Item -ItemType File -Path $F1 -Force | Out-Null
-    $F2 = "$PSScriptRoot\$CmDir\$CM2.zip".Replace("\", "/")
+    $F2 = "$dstDir\$CM2.zip".Replace("\", "/")
     New-Item -ItemType File -Path $F2 -Force | Out-Null
     # 打包差異的檔案
     $diff_list = "`$(git diff --name-only $CM1 $CM2)"
@@ -20,8 +28,8 @@ function getCommitFile {
     Invoke-Expression $cmd
     # 建立差異清單檔案
     $FileContent = $(git diff --name-only $CM1 $CM2)
-    Set-location $PSScriptRoot\$CmDir
-    $FileContent | Out-File -Encoding ASCII "diff-list.txt"
+    Set-location $dstDir
+    $FileContent | Out-File -Encoding ASCII $ListFileName
     # 解壓縮並刪除檔案
     if ($Expand) {
         Expand-Archive $F1 -Force; Remove-Item $F1
@@ -29,6 +37,7 @@ function getCommitFile {
     }
     # 恢復工作目錄
     Set-location $PSScriptRoot
+    return @("$dstDir\$CM1", "$dstDir\$CM2", "$dstDir\$ListFileName")
 }
 # ===========================================================
 function WinMergeU_Report {
@@ -41,57 +50,54 @@ function WinMergeU_Report {
     New-Item -ItemType File -Path $Output -Force | Out-Null
     WinMergeU $F1 $F2 -cfg Settings/ShowIdentical=0 -cfg Settings/DiffContextV2=$line -minimize -noninteractive -u -or $Output
 }
-
 function WinMergeU_Report2 {
     param (
         [string] $D1,
         [string] $D2,
-        [System.Object] $List
+        [System.Object] $List,
+        [string] $OutDir,
+        [switch] $Mode_S
     )
-    $OutDir = "D:/Work/WinMerg/java/WinMergOut"
-    foreach ($i in $List) {
-        $_D1 = $D1 + "/" + $i
-        $_D2 = $D2 + "/" + $i
-        
-        $MainDir = $i.Substring(0, $i.IndexOf("/"))
-        $idx = $i.LastIndexOf("/") + 1
-        $FileName = $i.Substring($idx, $i.Length - $idx )
-        
-        # $_Name = $OutDir + "/" + $i + ".html"
-        $_Name = $OutDir + "/" + $MainDir + "/" + $FileName + ".html"
-
-        $MainDir + "/" + $FileName
-
-        # WinMergeU_Report $_D1 $_D2 $_Name
-        # Write-Host WinMergeU_Report $_D1 $_D2 $_Name
+    if ($OutDir -eq "") { $OutDir = "$PSScriptRoot\WinMergOut" }
+    foreach ($item in $List) {
+        # 獲取兩個資料夾原始檔
+        $_D1 = $D1 + "\" + $item
+        $_D2 = $D2 + "\" + $item
+        # 簡化路徑輸出檔案路徑
+        if ($Mode_S) {
+            $MainDir = $item.Substring(0, $item.IndexOf("/"))
+            $idx=$item.LastIndexOf("/") + 1
+            $FileName = $item.Substring($idx, $item.Length - $idx )
+            $outName = $OutDir + "\" + $MainDir + "\" + $FileName + ".html"
+        } else {
+            $outName = $OutDir + "\" + $item + ".html"
+        }
+        # 輸出比對檔案
+        WinMergeU_Report $_D1 $_D2 $outName
     }
 }
-
-# $dir1 = "D:/Work/WinMerg/java/javaINIT"
-# $dir2 = "D:/Work/WinMerg/java/java1130" 
-# $List = @(
-#     "DMPZ01_WarProject/DMPZ01_WarProject/jsp/DMVX0050.html",
-#     "DMPZ01_WarProject/DMPZ01_WarProject/jsp/DMVXC001.inc",
-#     "DMPZ01_WarProject/DMPZ01_WarProject/jsp/DMVXS001.css",
-#     "DMPZDB/src/jp/co/hitachi_densa/DMPZDB/DMRZ013.java",
-#     "blcbatch_project/src/jp/co/Hitachi/soft/blc/wfvx/WFVX0250.java"
-# )
-# WinMergeU_Report2 $dir1 $dir2 $List
-
-function compareFile {
+function CompareDir {
     param (
         [string] $dir1,
-        [string] $dir2
+        [string] $dir2, 
+        [string] $ListFileName 
     )
-    
-    
+    $List = (Get-Content $ListFileName)
+    if ($List[0].ToString().Length -ge 20) {
+        WinMergeU_Report2 $dir1 $dir2 $List -Mode_S
+    } else {
+        # WinMergeU_Report2 $dir1 $dir2 $List
+    }
 }
+# ===========================================================
+
 # ===========================================================
 $gitDir = "Z:\gitRepo\doc_develop"
 $CM1 = "INIT"
 $CM2 = "master"
 
 # 獲取CommitFile
-getCommitFile $CM1 $CM2 $gitDir -Expand
+# $dir = getCommitFile $CM1 $CM2 $gitDir -Expand
 
-# 
+# 建立差異html
+CompareDir $dir[0] $dir[1] $dir[2]
