0e79471c8c4ae5dbbc4609600f65af4429d78ea6
Charlotte.Hong <hunandy14@gmail.com>
2021-12-03 18:09:27 +0900

    完成

--- compareDri.ps1
+++ compareDri.ps1
@@ -1,6 +1,6 @@
-# ===========================================================
+# ==================================================================================================
 function HTML_Head ($data){
-return '<!DOCTYPE html>
+    return '<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -11,7 +11,6 @@ return '<!DOCTYPE html>
 <body>
 ' + $data + "`n</body>`n</html>`n"
 }
-
 function HTML_Tag {
     param (
         [Parameter(Position = 0)]
@@ -29,68 +28,8 @@ function HTML_Tag {
     if($href -ne "") {$href = " href=`"$href`""}
     if($Attributes -ne "") {$Attributes = " $Attributes"}
     return "<$name$style$href$Attributes>$value</$name>"
-}
-# (HTML_Head (HTML_Tag "div" (HTML_Tag "a" "Link" -h "https://www.google.com.tw/") -s "width: 100px")) > index.html
-
-
-
-# ===========================================================
-function createIndexHTML {
-    param (
-        [string] $site,
-        [string] $Mode_S ,
-        [string] $diffList,
-        [string] $indexFileName
-    )
-    Set-Location $cmprOutDir
-    $docList = Get-Content $diffList
-
-    # 開頭
-    $Content = "" +
-    '<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-    '
-    # 產生各個項目
-    for ($i = 0; $i -lt $docList.Count; $i++) {
-        $item = $docList[$i]
-        if ($Mode_S) {
-            $MainDir = $item.Substring(0, $item.IndexOf("/"))
-            $idx = $item.LastIndexOf("/") + 1
-            $FileName = $item.Substring($idx, $item.Length - $idx )
-            $item2 = $MainDir + "/" + $FileName
-            $address = $site + "/" + $item2 + ".html"
-            $linkName = $item2
-        }
-        else {
-            $address = $site + "/" + $item + ".html"
-            $linkName = $item
-        }
-        
-        $Number = '<span style="width: 30px;display: inLine-block;">' + ($i + 1) + '</span>'
-
-        $out = '<div style="height: 22px">' + $Number + '<a href="' + $address + '">' 
-        $out = $out + '' + $linkName + ''
-        $out = $out + '</a></div>'
-        # Write-Host $out
-
-        $Content = $Content + $out + "`n"
-    }
-    # 結尾
-    $Content = $Content + '
-    </body>
-    </html>
-    '
-    # 輸出
-    [System.IO.File]::WriteAllLines($indexFileName, $Content);
-}
-# ===========================================================
+} # (HTML_Head (HTML_Tag "div" (HTML_Tag "a" "Link" -h "https://www.google.com.tw/") -s "width: 100px"))
+# ==========================================================
 function WinMergeU_Core {
     param (
         [Parameter(Position = 0)]
@@ -112,15 +51,20 @@ function WinMergeU_Core {
 }
 function WinMergeU_Dir {
     param (
+        [Parameter(Position = 0)]
         [string] $dir1,
+        [Parameter(Position = 1)]
         [string] $dir2,
+        [Parameter(Position = 2)]
         [System.Object] $listFileName,
+        [Parameter(Position = 3, ParameterSetName = "")]
         [string] $outDir,
+        [Parameter(ParameterSetName = "")]
+        [Int16] $Line = 3,
         [switch] $Mode_S
     )
-    $AddreseList  = @()
-    $FileNameList = @()
-    
+    $Content = ""
+    $idx = 1
     $collection = (Get-Content $listFileName)
     foreach ($item in $collection) {
         # 獲取兩個資料夾原始檔
@@ -130,35 +74,36 @@ function WinMergeU_Dir {
         if ($Mode_S) {
             $MainDir = $item.Substring(0, $item.IndexOf("/"))
             $idx = $item.LastIndexOf("/") + 1
-            $FileName = $item.Substring($idx, $item.Length - $idx )
-            $outName = $outDir + "\" + $MainDir + "\" + $FileName + ".html"
+            $FileName = "$MainDir\" + $item.Substring($idx, $item.Length - $idx )
         }
         else {
-            $outName = $outDir + "\" + $item.Replace("/", "\") + ".html"
+            $FileName = $item.Replace("/", "\")
         }
         # 輸出比對檔案
-        WinMergeU_Core $F1 $F2 $outName -Line 3
+        $outName = "$outDir\$FileName.html"
+        WinMergeU_Core $F1 $F2 $outName -Line $Line
+        
+        # HTML項目
+        $number = HTML_Tag "span" $idx -style "width: 30px;display: inline-block"
+        $link = HTML_Tag "a" $FileName -href $outName
+        $div = HTML_Tag "div" "`n    $number`n    $link`n" -style "height: 22px"
+        $Content = $Content + "$div`n"
+        $idx = $idx+1
     }
+    [System.IO.File]::WriteAllLines("$outDir\index.html", (HTML_Head $Content));
 }
-# ===========================================================
+# ==================================================================================================
 if ($PSScriptRoot) { $curDir = $PSScriptRoot } else { $curDir = (Get-Location).Path }
 $diffDir  = "Z:\autoCompare\doc_develop_update"
-$outDir   = "Z:\autoCompare\doc_develop_diff"
 $CM1      = "INIT"
 $CM2      = "master"
 $diffList = "diff-list.txt"
-
+$outDir   = "Z:\autoCompare\doc_develop_diff"
 
 $dir1   = "$diffDir\$CM1"
 $dir2   = "$diffDir\$CM2"
 $list   = "$diffDir\$diffList"
 $outDir = "$outDir"
-# WinMergeU_Dir $dir1 $dir2 $list $outDir
 
-
-# 建立indexJTML
-# $diffList = $dir[2]
-# $indexFileName = "$cmprOutDir\index.html"
-# createIndexHTML $cmprOutDir $Mode_S $diffList $indexFileName
-
-# ===========================================================
+WinMergeU_Dir $dir1 $dir2 $list -outDir $outDir -Line 3
+# ==================================================================================================
