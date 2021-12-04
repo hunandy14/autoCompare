a806785352ce4037f8266b9de27c22e90b7639ea
Charlotte.Hong <hunandy14@gmail.com>
2021-12-04 17:29:50 +0900

    完成 剩下小細節修整

--- compareGit.ps1
+++ compareGit.ps1
@@ -12,56 +12,51 @@ function compareGitCommit {
         [Parameter(ParameterSetName = "")]
         $projectName
     )
+    # ===================================================
+    # 載入函式
     # Import-module .\archiveGit.ps1
     # Import-module .\compareDri.ps1
     irm "https://raw.githubusercontent.com/hunandy14/autoCompare/master/archiveGit.ps1" | iex
     irm "https://raw.githubusercontent.com/hunandy14/autoCompare/master/compareDri.ps1" | iex
-    $listFileName = "diff-list.txt"
     # ===================================================
-    # 輸出 WinMerge 比較清單
+    # 初始化設定
+    $listFileName = "diff-list.txt"
+    # 決定 outDir 子資料夾增加與否的邏輯
     if ($PSScriptRoot) { $curDir = $PSScriptRoot } else { $curDir = (Get-Location).Path }
-    $list = getCommitDiff $CM1 $CM2 -gitDir $gitDir
-
     if ($projectName) {
         if (!$outDir) { $outDir = $curDir }
         $outDir = "$outDir\$projectName"
     } elseif (!$outDir) { $outDir = "$curDir\compare_source" }
-    
-    # if ($projectName) { $outDir = "$outDir\$projectName" }
-    # 書出檔案
-    # archiveCommit $CM1 $List $gitDir -outFile "source_before.zip" -outDir $outDir -Expand
-    # archiveCommit $CM2 $List $gitDir -outFile "source_after.zip"  -outDir $outDir -Expand
-    # 輸出清單
-    # $listFileName = "$outDir\$listFileName"
-    # [System.IO.File]::WriteAllLines($listFileName, $list);
-    
     # ===================================================
-    $outDir
-    return
+    # 從git提交點中獲取檔案
+    $list = getCommitDiff $CM1 $CM2 -gitDir $gitDir
+    archiveCommit $CM1 $List $gitDir -outFile "source_before.zip" -outDir $outDir -Expand
+    archiveCommit $CM2 $List $gitDir -outFile "source_after.zip"  -outDir $outDir -Expand
+    [System.IO.File]::WriteAllLines("$outDir\$listFileName", $list);
     # ===================================================
-    $diffDir = "Z:\Work\doc_diff"
+    $srcDir  = $outDir
 
     $dir1    = "source_after"
     $dir2    = "source_before"
-    $list    = "diff-list.txt"
-    $outDir  = "source_cmp"
+    $list    = $listFileName
+    $repDir  = "source_cmp"
 
-    $dir1    = "$diffDir\$dir1"
-    $dir2    = "$diffDir\$dir2"
-    $list    = "$diffDir\$list"
-    $outDir  = "$diffDir\$outDir"
-
-    # WinMergeU_Dir $dir1 $dir2 $list -outDir $outDir -Line 3 -Mode_S
+    $dir1    = "$srcDir\$dir1"
+    $dir2    = "$srcDir\$dir2"
+    $list    = "$srcDir\$list"
+    $outDir2 = "$srcDir\$repDir"
+    WinMergeU_Dir $dir1 $dir2 $list -outDir $outDir2 -Line 3 -Mode_S
 }
 # ==================================================================================================
-$CM1         = "master"
-$CM2         = "INIT"
-$gitDir      = "Z:\gitRepo\doc_develop"
-$outDir      = "Z:\work"
-
-# compareGitCommit $CM1 $CM2 $gitDir -outDir $outDir
-
-compareGitCommit $CM1 $CM2 $gitDir -outDir $outDir
-compareGitCommit $CM1 $CM2 $gitDir -outDir $outDir -projectName "doc_1130"
-compareGitCommit $CM1 $CM2 $gitDir 
-compareGitCommit $CM1 $CM2 $gitDir                 -projectName "doc_1130"
+# 使用範例
+function test_compareGit {
+    $CM1         = "master"
+    $CM2         = "INIT"
+    $gitDir      = "Z:\gitRepo\doc_develop"
+    $outDir      = "Z:\work"
+    
+    compareGitCommit $CM1 $CM2 $gitDir -outDir $outDir
+    # compareGitCommit $CM1 $CM2 $gitDir -outDir $outDir -projectName "doc_1130"
+    # compareGitCommit $CM1 $CM2 $gitDir 
+    # compareGitCommit $CM1 $CM2 $gitDir                 -projectName "doc_1130"
+}
