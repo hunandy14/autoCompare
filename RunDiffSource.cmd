7af512f17e254a434bd59e021baf9a3ed5f1b6be
Charlotte.Hong <hunandy14@gmail.com>
2022-11-23 20:13:05 +0900

    追加啟動檔案

---
+++ RunDiffSource.cmd
@@ -0,0 +1,28 @@
+@echo off
+
+set "0=%~f0"& set "1=%~dp0"& set PwshScript=([Io.File]::ReadAllText($env:0,[Text.Encoding]::Default) -split '[:]PwshScript')
+powershell -nop -c "(%PwshScript%[2]+%PwshScript%[1])|iex; Exit $LastExitCode"
+
+echo ExitCode: %errorlevel%& pause
+Exit %errorlevel%
+
+
+:PwshScript#:: script1
+#:: --------------------------------------------------------------------------------------------------------------------------------
+Write-Host "by PSVersion::" $PSVersionTable.PSVersion "`n"
+
+# 設定
+$LeftPath  = "before"
+$RightPath = "after"
+$OutPath   = "Report\index.html"
+$List      = "list.txt"
+
+# 比較
+DiffSource $LeftPath $RightPath -Output $OutPath -Include (Get-Content $List)
+
+
+
+:PwshScript#:: script2
+#:: --------------------------------------------------------------------------------------------------------------------------------
+Set-Location ($env:1); [IO.Directory]::SetCurrentDirectory(((Get-Location -PSProvider FileSystem).ProviderPath))
+irm raw.githubusercontent.com/hunandy14/autoCompare/master/DiffSource.ps1|iex
