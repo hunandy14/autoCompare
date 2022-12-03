47ada6fde04b00a453e7ffda8f36ebb4c0f4c284
Charlotte.Hong <hunandy14@gmail.com>
2022-12-03 22:46:21 +0900

    補完空Comitt代用代碼

--- ArchiveGitCommit.ps1
+++ ArchiveGitCommit.ps1
@@ -62,18 +62,22 @@ function diffCommit {
 # 從指定提交點取出特定清單檔案
 function archiveCommit {
     param (
-        [Parameter(Position = 0, ParameterSetName = "", Mandatory)]
+        [Parameter(Position = 0, ParameterSetName = "")]
         [string] $Commit,
         [Parameter(Position = 1, ParameterSetName = "")]
         [object] $List,
         [Parameter(ParameterSetName = "")]
         [string] $Output, # 預設為 "$gitDirName-$Commit.zip"
-                          #   1. Output為Zip: "$Output.zip"
-                          #   2. Output為Dir: "$Output\$gitDirName-$Commit.zip"
+                          #   A. Output為Zip: 保持手動"$Output.zip"
+                          #   B. Output為Dir: 檔名自動"$Output\$gitDirName-$Commit.zip"
         [Parameter(ParameterSetName = "")]
         [string] $Path,  # 預設為當前工作目錄
-        [switch] $Expand # 1. 路徑為目錄: 直接輸出檔案到目錄
-                         # 2. 路徑為Zip : 原地解壓縮Zip
+        [switch] $Expand # A. 路徑為目錄:
+                         #     1. 輸出檔案到    [指定路徑] (            )
+                         #     0. 輸出Zip       [        ] (自動檔名.zip)
+                         # B. 路徑為Zip : 
+                         #     1. 原地解壓縮Zip [指定路徑] (自訂檔名.Zip)
+                         #     0. 輸出Zip       [        ] (自訂檔名.Zip)
     )
     # 檢測路徑
     [IO.Directory]::SetCurrentDirectory(((Get-Location -PSProvider FileSystem).ProviderPath))
@@ -82,9 +86,10 @@ function archiveCommit {
         if (!(Test-Path -PathType:Container "$Path\.git")) { Write-Error "Error:: The path `"$Path`" is not a git folder" -ErrorAction:Stop }
     } else { $Path = Get-Location}
     
-    # 設置路徑與
+    
+    # 設置路徑
     $gitDirName = (Split-Path $Path -Leaf)
-    if (!$Commit) { $Commit = "HEAD" }
+    if (!$Commit) { $Commit = "HEAD"; $CommitIsNull=$true }
     $defDstName = "$gitDirName-$Commit.zip"
     if ($Output) { # 有路徑且為資料夾時創建自動檔名
         if (!(Split-Path $Output -Extension)) {
@@ -95,45 +100,106 @@ function archiveCommit {
             } else {
                 $Output = "$Output\$defDstName"
             }
-        }
+        } else { $OutputIsDir = $false }
     } else { # 路徑為空
         $Output = $defDstName
     } $Output = [System.IO.Path]::GetFullPath($Output)
-    # 設置命令
-    $cmd = ("git archive -o '$Output' $Commit $List").Trim()
-    # 打包差異的檔案
-    if ($cmd) {
-        if ($Path) { $curDir = (Get-Location).Path; Set-location $Path }
-        $dstDir = (Split-Path $Output -Parent)
-        if (!(Test-Path $dstDir)) { New-Item -ItemType Directory $dstDir  -Force | Out-Null }
-        Invoke-Expression $cmd
-        # Write-Host $cmd -ForegroundColor:Yellow
-        if ($Path) { Set-location $curDir }
-    }
-    # 解壓縮並刪除檔案
-    if ($Expand) {
-        if ($OutputIsDir) { # 解壓縮到目標資料夾並刪除 zip 檔案
-            $ExpPath = Split-Path $Output
-            $ExpPath = [System.IO.Path]::GetFullPath($ExpPath)
-            if ($ExpPath -eq $Path) { $Output=$null; Write-Error "The `$Output location is the same as the Git directory." } else {
-                Expand-Archive $Output $ExpPath -Force
-                if ((Split-Path $Output -Leaf) -eq "archiveCommit-temp.zip") { Remove-Item $Output } # 多餘的if判斷避免砍錯檔案
-                $Output = $ExpPath
+    
+    
+    # 從git目錄壓縮特定檔案成壓縮包 (git不支援從當前狀態取檔)
+    if ($CommitIsNull) {
+        # 設定目錄
+        if ($Expand) { # 複製到指定路徑
+            if ($OutputIsDir) {
+                $CopyTemp = Split-Path $Output -Parent
+            } else { # 複製到指定路徑(包含zip檔名)
+                $CopyTemp = (Split-Path $Output -Parent)+"\"+(Split-Path $Output -LeafBase)
             }
-        } else { # 僅解壓縮
-            $ExpPath = "$(Split-Path $Output)\$(Split-Path $Output -LeafBase)"
-            $ExpPath = [System.IO.Path]::GetFullPath($ExpPath)
-            if ($ExpPath -eq $Path) { Write-Warning "The unzip location is the same as the Git directory, Program will not decompress." } else {
-                Expand-Archive $Output $ExpPath -Force
+            if ((Test-Path $CopyTemp) -and (Get-ChildItem $CopyTemp)) { Write-Warning "Output directory is not an empty directory, the output may be mixed with other files." }
+        } else { # 複製到暫存路徑
+            $CopyTemp = "$env:TEMP\archiveCommitTemp"
+            if (Test-Path "$env:TEMP\archiveCommitTemp\*") { Remove-Item "$env:TEMP\archiveCommitTemp\*" -Recurse }
+        }
+        # 複製差異檔案到暫存目錄
+        $FileInfo = (Get-ChildItem -Path:$Path -Include:$List -Recurse -File)
+        ($FileInfo.FullName)|ForEach-Object{
+            $RelPath = [IO.Path]::GetRelativePath($Path, $_)
+            $F1 = $_; $F2 = "$CopyTemp\$RelPath"
+            $ParentPath = (Split-Path $F2 -Parent)
+            if (!(Test-Path $ParentPath)) { New-Item $ParentPath -ItemType:Directory -Force |Out-Null }
+            # Write-Host $F1; Write-Host "  ->" $F2
+            Copy-Item $F1 $F2
+        }
+        # 壓縮檔案
+        if ($OutputIsDir) {
+            if (!$Expand) {             # [複製到暫存路徑, 壓縮在指定路徑(自動檔名.Zip)]
+                if (!(Test-Path (Split-Path $Output -Parent))) { New-Item $Output -ItemType:Directory -Force |Out-Null }
+                Compress-Archive -Path "$CopyTemp\*" -DestinationPath "$Output" -Force
+                return $Output
+            }else {                     # [複製到指定路徑]
+                return $CopyTemp
+            }
+        } else {
+            if (!$Expand) {             # [複製到暫存路徑, 壓縮在指定路徑(自定檔名.Zip)]
+                if (!(Test-Path (Split-Path $Output -Parent))) { New-Item $Output -ItemType:Directory -Force |Out-Null }
+                Compress-Archive "$CopyTemp\*" $Output -Force
+                return $Output
+            } else {                    # [複製到指定路徑2, 壓縮在指定路徑(自定檔名.Zip)]
+                if (!(Test-Path (Split-Path $Output -Parent))) { New-Item $Output -ItemType:Directory -Force |Out-Null }
+                Compress-Archive "$CopyTemp\*" $Output -Force
+                return $CopyTemp
             }
         }
-    } return $Output
+        
+    # 從git倉庫獲取檔案壓縮包
+    } else {
+        # 打包差異的檔案
+        $cmd = ("git archive -o '$Output' $Commit $List").Trim()
+        if ($cmd) {
+            # 執行命令
+            if ($Path) { $curDir = (Get-Location).Path; Set-location $Path }
+            $dstDir = (Split-Path $Output -Parent)
+            if (!(Test-Path $dstDir)) { New-Item -ItemType Directory $dstDir  -Force | Out-Null }
+            Invoke-Expression $cmd
+            # Write-Host $cmd -ForegroundColor:Yellow
+            if ($Path) { Set-location $curDir }
+        }
+        # 解壓縮並刪除檔案
+        if ($Expand) {
+            if ($OutputIsDir) { # 解壓縮到目標資料夾並刪除 zip 檔案
+                $ExpPath = Split-Path $Output
+                $ExpPath = [System.IO.Path]::GetFullPath($ExpPath)
+                if ($ExpPath -eq $Path) { $Output=$null; Write-Error "The `$Output location is the same as the Git directory." } else {
+                    Expand-Archive $Output $ExpPath -Force
+                    if ((Split-Path $Output -Leaf) -eq "archiveCommit-temp.zip") { Remove-Item $Output } # 多餘的if判斷避免砍錯檔案
+                    $Output = $ExpPath
+                }
+            } else { # 僅解壓縮
+                $ExpPath = "$(Split-Path $Output)\$(Split-Path $Output -LeafBase)"
+                $ExpPath = [System.IO.Path]::GetFullPath($ExpPath)
+                if ($ExpPath -eq $Path) { Write-Warning "The unzip location is the same as the Git directory, Program will not decompress." } else {
+                    Expand-Archive $Output $ExpPath -Force
+                }
+            }
+        } return $Output
+    }
 } # archiveCommit HEAD @("*.css") -Path:"Z:\doc" -Output:"$env:TEMP\archiveCommit"
 
 # archiveCommit HEAD @("*.css") -Path:"Z:\doc" -Output:"$env:TEMP\archiveCommit\doc-HEAD" -Expand
 # archiveCommit HEAD @("*.css") -Path:"Z:\doc" -Output:"$env:TEMP\archiveCommit\Archive.zip" -Expand
 # archiveCommit HEAD @("*.css") -Path:"Z:\doc" -Output:"$env:TEMP\archiveCommit"
 # archiveCommit HEAD @("*.css") -Path:"Z:\doc" -Output:"$env:TEMP\archiveCommit\Archive.zip"
+# 空Comit測試
+# archiveCommit "" EAWD1100.css, EAWD1100.js -Path:"Z:\doc" -Output:"$env:TEMP\archiveCommit\Archive.zip"
+# archiveCommit "" EAWD1100.css, EAWD1100.js -Path:"Z:\doc" -Output:"$env:TEMP\archiveCommit"
+# archiveCommit "" EAWD1100.css, EAWD1100.js -Path:"Z:\doc"
+# Expand測試
+# archiveCommit "" EAWD1100.css, EAWD1100.js -Path:"Z:\doc"
+# archiveCommit "" EAWD1100.css, EAWD1100.js -Path:"Z:\doc" -Output:"archiveCommit.zip"
+# archiveCommit "" EAWD1100.css, EAWD1100.js -Path:"Z:\doc" -Output:"$env:TEMP\archiveCommit"
+# archiveCommit "" EAWD1100.css, EAWD1100.js -Path:"Z:\doc" -Output:"$env:TEMP\archiveCommit\doc" -Expand
+# archiveCommit "" EAWD1100.css, EAWD1100.js -Path:"Z:\doc" -Output:"$env:TEMP\archiveCommit\Archive.zip"
+# archiveCommit "" EAWD1100.css, EAWD1100.js -Path:"Z:\doc" -Output:"$env:TEMP\archiveCommit\Archive.zip" -Expand
 # 例外測試
 # archiveCommit -Output:(Get-Location) HEAD -Expand
 # archiveCommit HEAD @("*.css") -Path:"Z:\doc" -Output:"Z:\doc" -Expand
