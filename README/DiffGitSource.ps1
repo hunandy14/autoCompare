8bce555acc8cc2dfd3db4b5dae932a39146e2319
Charlotte.Hong <hunandy14@gmail.com>
2022-12-04 16:17:25 +0900

    新增轉發文件

---
+++ README/DiffGitSource.ps1
@@ -0,0 +1,3 @@
+# 轉發函式載入清單
+Invoke-RestMethod "https://raw.githubusercontent.com/hunandy14/autoCompare/master/ArchiveGitCommit.ps1"|Invoke-Expression
+Invoke-RestMethod "https://raw.githubusercontent.com/hunandy14/autoCompare/master/DiffSource.ps1"|Invoke-Expression
