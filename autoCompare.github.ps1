40b18770f9bd4b6e6cc8b8760c810803faf92f23
Charlotte.Hong <hunandy14@gmail.com>
2024-12-24 14:01:23 +0900

    新增線上版動態載入範例

---
+++ autoCompare.github.ps1
@@ -0,0 +1,20 @@
+$PSJwtModuleHome = 'https://raw.githubusercontent.com/hunandy14/autoCompare/refs/heads/dev/2.0/src'
+
+$scriptPaths = @(
+    "decodeOctal.ps1",
+    "archiveFiles.ps1",
+    "diffCommit.ps1"
+)
+
+foreach ($scriptPath in $scriptPaths) {
+    try {
+        # Loding function in memory
+        $irmParams = @{
+            Uri = "$PSJwtModuleHome/$scriptPath"
+        }; if (![string]::IsNullOrWhiteSpace($env:HTTP_PROXY)) {
+            $irmParams['Proxy'] = $env:HTTP_PROXY
+        }; Invoke-RestMethod @irmParams | Invoke-Expression -EA Stop
+    } catch {
+        Write-Error "$($_.Exception.Message)" -ea 1
+    }
+}
